e62f7fb5c6ee4211c8b66240de5443f296916b29
Alice <sten123465@gmail.com>
2024-12-25 19:45:45 +0100

    add- api/number

---
+++ api/number/decrement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.number
+{
+	public class decrement : IPost
+	{
+		public string HandlePOST(string requestBody, out int statusCode, out string message)
+		{
+			StateManagement.Add(-1);
+			statusCode = 200;
+			message = "OK";
+			return StateManagement.GetNumber().ToString();
+		}
+	}
+}
