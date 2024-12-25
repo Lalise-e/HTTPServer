d7e031ad2410ea5d0d8d030f58ff185d7cbf3af0
Alice <sten123465@gmail.com>
2024-12-25 17:58:37 +0100

    add- api/hello-world/print

---
+++ api/hello-world/print.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.hello_world
+{
+	internal class print : IGet
+	{
+		public string HandleGet(string requestBody, out int statusCode, out string message)
+		{
+			statusCode = 200;
+			message = "OK";
+			return "Hello, World!";
+		}
+	}
+}
