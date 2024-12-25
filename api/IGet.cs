976bfce0d27b1fd2fee09e8b2be833ef9fc2191a
Alice <sten123465@gmail.com>
2024-12-25 17:57:51 +0100

    add- HandleGet to IGet

--- api/IGet.cs
+++ api/IGet.cs
@@ -11,6 +11,13 @@ namespace api
 	/// </summary>
 	public interface IGet
 	{
-
+		/// <summary>
+		/// Handles an API GET request.
+		/// </summary>
+		/// <param name="requestBody">The body of the HTTP request.</param>
+		/// <param name="statusCode">The status code of the response.</param>
+		/// <param name="message">Status message</param>
+		/// <returns>The response, will eventually be a json or something, idk haven't decided yet.</returns>
+		public string HandleGet(string requestBody, out int statusCode, out string message);
 	}
 }
