2673a243ffc6e8e04fefff38c45544fe1904ac59
Alice <sten123465@gmail.com>
2024-12-25 19:44:38 +0100

    add- HandlePOST to IPost

--- api/IPost.cs
+++ api/IPost.cs
@@ -11,5 +11,13 @@ namespace api
 	/// </summary>
 	public interface IPost
 	{
+		/// <summary>
+		/// Handles an API GET request.
+		/// </summary>
+		/// <param name="requestBody">The body of the HTTP request.</param>
+		/// <param name="statusCode">The status code of the response.</param>
+		/// <param name="message">Status message</param>
+		/// <returns>The response, will eventually be a json or something, idk haven't decided yet.</returns>
+		public string HandlePOST(string requestBody, out int statusCode, out string message);
 	}
 }
