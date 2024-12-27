90e5e208a942fbc112df5a3a2524d9806ea6a0cf
Alice <sten123465@gmail.com>
2024-12-27 01:04:37 +0100

    remove- api project, this should be solution specific. Will add an example as a seperate branch or something

---
+++ api/test/hello_world.cs
@@ -0,0 +1,16 @@
+﻿using HTTPFramework;
+using System.Text;
+
+namespace api.test
+{
+	internal class hello_world : IApiCall
+	{
+		public HTTPMethod TargetMethod { get { return HTTPMethod.GET; } }
+
+		public byte[] HandleCall(HTTPRequest request, out string contentType)
+		{
+			contentType = "content-type: text/plain";
+			return Encoding.UTF8.GetBytes("Hello, World!");
+		}
+	}
+}
